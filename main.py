2f9d606fba742314737feb927ab3d3cfd6c96f67
Max Kozlov <masterkotlet007@gmail.com>
2023-12-16 10:08:09 +0300

    +Научный полк отдельно+массив людей

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 from telebot import types
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from telebot.types import InputMedia
 
 photo_url = 'https://www.ystu.ru/upload/iblock/123/13sj41of6u5r83r7aam4stp5ej7z4ad2/DSC_3320.JPG'
@@ -8,13 +9,27 @@ photo = '/img/1.jpg'
 
 bot = telebot.TeleBot('6825371641:AAH6vlXvmneuJqFfiEwzrDtn0EokwJBUJz4')
 
-@bot.message_handler    (commands=['start'])
-def main(message):
-    bot.send_message(message.chat.id, f'Здравствуйте,{message.from_user.first_name}. Рады вас видеть. '
-                                      'Мы готовы провести замечательную экскурсию по '
-                                      'нашему музею ЯГТУ. Ваш помощник - <em> <b>/help </b> </em>', parse_mode='html')
+people_data = [
+{
+"name": "",
+"photo": "",
+"description": ""
+},
+{
+"name": "",
+"photo": "",
+"description": "."
+},
+{
+"name": "",
+"photo": "",
+"description": ""
+}
+]
 
-@bot.message_handler    (commands=['help'])
+
+
+@bot.message_handler(commands=['help'])
 def main(message):
     markup = types.InlineKeyboardMarkup()
     key = types.InlineKeyboardMarkup()
@@ -28,22 +43,17 @@ def main(message):
     markup.row(but_3,but_4)
     but_5 = types.InlineKeyboardButton(text="Информация", callback_data="info")
     markup.row(but_5)
-    but_6 = types.InlineKeyboardButton(text="Значимые личности ЯГТУ", callback_data="people")
+    but_6 = types.InlineKeyboardButton(text="Научный полк ЯГТУ", callback_data="scientists")
     markup.row(but_6)
     key.add(but_1, but_2, but_3, but_4,but_5,but_6)
     bot.send_message(message.chat.id, "Навигация", reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda callback: True)
+
 def callback_message(callback):
     if callback.data == 'history':
-        bot.send_photo(callback.message.chat.id, photo=photo_url1, caption = 'В 1944 году в Ярославле был создан технологический институт резиновой промышленности.'
-                                                                             ' Директором вуза тогда был назначен Евгений Васильевич Мигулин. '
-                                                                             'В кратчайшие сроки были образованы первые кафедры, собрано необходимое оборудование для лабораторий и произведен ремонт. В 1946 году институт из Министерства резиновой промышленности передали в ведение высшего образования СССР.'
-                                                                             'В апреле 1948 в вузе прошла первая студенческая конференция. '
-                                                                             'Первый выпуск специалистов состоялся в 1949 году. В 1950 году в вузе было открыто вечернее отделение. '
-                                                                             'В 1953 году вуз был преобразован в технологический.Вуз сделал значительный шаг в оснащении кафедр новым оборудованием, расширил перечень специальностей, увеличил контингент студентов. '
-                                                                             )
+        bot.send_photo(callback.message.chat.id, photo=photo_url1, caption = '+текст про историю музея')
     elif callback.data == 'back':
         bot.send_message(callback.message.chat.id, f'Пожалуйста, оставьте свой отзыв - это поможет сделать нас лучше и ещё больше людей узнают о музее ЯГТУ')
     elif callback.data == 'exs':
@@ -51,25 +61,28 @@ def callback_message(callback):
 
         but_6 = types.KeyboardButton(text="Общий вид музея")
 
-        but_7 = types.KeyboardButton(text="Секция 1")
-        but_8 = types.KeyboardButton(text="Секция 2")
-        but_9 = types.KeyboardButton(text="Секция 3")
-        but_10 = types.KeyboardButton(text="ГЕНПЛАН Политеха")
+        but_7 = types.KeyboardButton(text="Секция 1(1945-1953)")
+        but_8 = types.KeyboardButton(text="Секция 2(1953-1973)")
+        but_9 = types.KeyboardButton(text="Секция 3(1973-1994)")
+        but_10 = types.KeyboardButton(text="Три ГЕНПЛАНА Политеха")
         but_11 = types.KeyboardButton(text="Первые достижения")
-
-
-
-
         markup.add(but_6,but_7,but_8,but_9,but_10,but_11)
         bot.send_message(callback.message.chat.id, "Пожалуйста, выберите из меню(снизу) то что вы бы хотели увидеть", reply_markup=markup)
     elif callback.data == 'info':
         bot.send_photo(callback.message.chat.id, photo=photo_url, caption = f'Расписание и расположение музея \n Музей располагается по адресу Московский проспект, 88, Ярославль в А корпусе ЯГТУ. Попасть в него можно придупредив(связаться) с Поповым  Романом Игоревичем.' )
-    elif callback.data == 'people':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        but_12 = types.KeyboardButton(text="Значимые личности ЯГТУ(Часть 1)")
-        but_13 = types.KeyboardButton(text="Значимые личности ЯГТУ(Часть 2)")
-        markup.add(but_12,but_13)
-        bot.send_message(callback.message.chat.id,"Пожалуйста выберите из меню",  reply_markup=markup)
+    elif callback.data == 'scientists':
+        markup = types.InlineKeyboardMarkup()
+        people_buttons = []
+        for person in people_data:
+            button = types.InlineKeyboardButton(text=person['name'], callback_data=person['name'])
+            people_buttons.append(button)
+        markup.add(*people_buttons)
+        bot.send_message(callback.message.chat.id, "Выберите человека из научного полка:", reply_markup=markup)
+    else:
+        for person in people_data:
+            if callback.data == person['name']:
+                bot.send_photo(callback.message.chat.id, photo=person['photo'], caption=person['description'])
+                break
 
 
 @bot.message_handler(content_types=['text'])
@@ -78,44 +91,26 @@ def bot_message(message):
         if message.text == 'Общий вид музея':
             bot.send_photo(message.chat.id, photo=open('1.jpg', 'rb'),
                        caption='Представляет вашему вниманию общий вид на экспонаты')
-        elif message.text == 'Секция 1':
+        elif message.text == 'Секция 1(1945-1953)':
             bot.send_photo(message.chat.id, photo=open('1.1.jpg', 'rb'), caption='Первая секция')
-        elif message.text == 'Секция 2':
+        elif message.text == 'Секция 2(1953-1973)':
             bot.send_photo(message.chat.id, photo=open('2.jpg', 'rb'), caption='Вторая секция')
-        elif message.text == 'Секция 3':
+        elif message.text == 'Секция 3(1973-1994)':
             bot.send_photo(message.chat.id, photo=open('3.jpg', 'rb'), caption='Третья секция')
-        elif message.text == 'ГЕНПЛАН Политеха':
-            bot.send_media_group(message.chat.id, [telebot.types.InputMediaPhoto(open('4.jpg', 'rb'),caption='ГЕНПЛАН Политеха'),
-                                                   telebot.types.InputMediaPhoto(open('5.jpg', 'rb'))])
+        elif message.text == 'Три ГЕНПЛАНА Политеха':
+            bot.send_media_group(message.chat.id, [telebot.types.InputMediaPhoto(open('4.jpg', 'rb'),caption='ГЕНПЛАН Политеха.'),
+                                                   telebot.types.InputMediaPhoto(open('5.jpg', 'rb')),
+                                                   telebot.types.InputMediaPhoto(open('10.jpg', 'rb'))])
         elif message.text == 'Первые достижения':
             bot.send_media_group(message.chat.id,
                                  [telebot.types.InputMediaPhoto(open('6.jpg', 'rb'), caption='Первые достижения'),
                                   telebot.types.InputMediaPhoto(open('7.jpg', 'rb')),
                                   telebot.types.InputMediaPhoto(open('7.jpg', 'rb'))])
-        elif message.text == 'Значимые личности ЯГТУ(Часть 1)':
-            bot.send_media_group(message.chat.id,
-                                 [
-                                  telebot.types.InputMediaPhoto(open('11.jpg', 'rb')),
-                                  telebot.types.InputMediaPhoto(open('12.jpg', 'rb')),
-                                  telebot.types.InputMediaPhoto(open('13.jpg', 'rb')),
-                                  telebot.types.InputMediaPhoto(open('14.jpg', 'rb')),
-                                  telebot.types.InputMediaPhoto(open('15.jpg', 'rb')),
-                                  telebot.types.InputMediaPhoto(open('16.jpg', 'rb')),
-                                  telebot.types.InputMediaPhoto(open('17.jpg', 'rb')),
-                                  telebot.types.InputMediaPhoto(open('18.jpg', 'rb')),
-                                  telebot.types.InputMediaPhoto(open('19.jpg', 'rb'))])
-        elif message.text == 'Значимые личности ЯГТУ(Часть 2)':
-            bot.send_media_group(message.chat.id,
-                                 [telebot.types.InputMediaPhoto(open('10.jpg', 'rb')),
-                                  telebot.types.InputMediaPhoto(open('21.jpg', 'rb')),
-                                  telebot.types.InputMediaPhoto(open('22.jpg', 'rb')),
-                                  telebot.types.InputMediaPhoto(open('23.jpg', 'rb')),
-                                  telebot.types.InputMediaPhoto(open('24.jpg', 'rb')),
-                                  telebot.types.InputMediaPhoto(open('25.jpg', 'rb')),
-                                  telebot.types.InputMediaPhoto(open('26.jpg', 'rb')),
-                                  telebot.types.InputMediaPhoto(open('27.jpg', 'rb')),
-                                  telebot.types.InputMediaPhoto(open('28.jpg', 'rb')),
-                                  telebot.types.InputMediaPhoto(open('29.jpg', 'rb'))])
+
+
+
+
+
 
 
 
